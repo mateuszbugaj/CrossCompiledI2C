094af6e332325ed980811cec5af72437a6ca18f7
Mateusz Bugaj <mateuszbugaj5@gmail.com>
2023-10-29 17:43:50 +0100

    Generating files for usage with SimpleLogicAnalyzer, tested one-way communication

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
+# Cross compiled I2C
+A demo/development platform for I2C protocol. </br>
+It can be run on a desktop by running two instances of the executable that communicate with each other using a JSON file or it can be build and uploaded to run on the AVR/STM32 microcontroller. </br>
+Each target platform is using the same version of the [I2C library](https://github.com/mateuszbugaj/GenericI2C) by compiling the appropriate HAL implementation.
+
+## Desktop platform
+Create the build-system
 ```
-cmake -S . -B build/ -DTARGET_PLATFORM=AVR -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain.cmake
+cmake -S . -B build-desktop/ -DTARGET_PLATFORM=DESKTOP
+```
+
+Build executable
+```
+cmake --build build-desktop/
+```
+
+Run `transmitter` instance
+```
+build-desktop/src/desktop/DesktopDemo --role=1
+```
+
+Run `receiver` instance
+```
+build-desktop/src/desktop/DesktopDemo --role=2
+```
+
+The HAL implementation is using `output/pin_states.json` instead of real MCU pins to read and write to the I2C bus. </br>
+Each new pin output generates new line which contains the state of pins and the calculated bus state.
+```JSON
+{"timestamp":"16:02:59.415","signals":[{"MASTER_SCL_OUT":0},{"MASTER_SDA_OUT":0},{"SLAVE_SCL_OUT":0},{"SLAVE_SDA_OUT":0}],"SCL":1,"SDA":1}
+```
+
+Other files in the `output/` directory such as `logic_analyzer_snapshot.txt`, `master_log.txt` or `slave_log.txt` can be used by [logic analyzer](https://github.com/mateuszbugaj/SimpleLogicAnalyzer).
+
+
+## AVR platform
+
+```
+cmake -S . -B build-avr/ -DTARGET_PLATFORM=AVR -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain.cmake
 cmake --build build-avr/
 sudo make -C build-avr/ upload_AvrDemo
 ```
 
-```
-cmake -S . -B build-desktop/ -DTARGET_PLATFORM=DESKTOP
-cmake --build build-desktop/
-build-desktop/src/desktop/DesktopDemo
-```
+## STM32 platform
--- src/desktop/DesktopDemo.cpp
+++ src/desktop/DesktopDemo.cpp
@@ -1,26 +1,132 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <vector>
+#include <map>
+#include <thread>
+#include <chrono>
 
+#include <rapidjson/document.h>
+#include <rapidjson/ostreamwrapper.h>
+#include <rapidjson/istreamwrapper.h>
+#include <rapidjson/prettywriter.h>
 
 #include <I2C_HAL.h>
 extern "C" {
   #include <I2C.h>
 }
 
+#define PIN_STATE_FILE "output/pin_states.json"
+#define LOGIC_ANALYZER_FILE "output/logic_analyzer_snapshot.txt"
+
+I2C_Role role = I2C_Role::MASTER;
+bool logicAnalyzerProbeRunning = false;
+
 void print_str(char str[]){
-  std::cout << str;
+  std::string filename = std::string("output/") + (role == I2C_Role::MASTER ? "master" : "slave") + "_log.txt";
+
+  char strCopy[50] {0};
+
+  // Strip trailing newlines
+  for(int i = 0; i < strlen(str); i++){
+    if(str[i] != '\r'){
+      strCopy[i] = str[i];
+    } else {
+      strCopy[i] = '\0';
+      break;
+    }
+  }
+
+  std::ofstream ofs(filename, std::ios::app);
+  if(ofs.is_open()){
+    ofs << strCopy;
+    ofs.close();
+  } else {
+    std::cout << "Unable to open file for writing log\n";
+  }
+
+  std::cout << strCopy;
 }
 
 void print_num(uint8_t num){
+  std::string filename = std::string("output/") + (role == I2C_Role::MASTER ? "master" : "slave") + "_log.txt";
+  std::ofstream ofs(filename, std::ios::app);
+  if(ofs.is_open()){
+    ofs << int(num);
+    ofs.close();
+  } else {
+    std::cout << "Unable to open file for writing log\n";
+  }
+
   std::cout << int(num);
+}
+
+void logicAnalyzerSnapshot(){
+  // Parse last line of the PIN_STATE_FILE to get the current pin states and save them to a LOGIC_ANALYZER_FILE for the logic analyzer.
+  // Each line of the PIN_STATE_FILE is a JSON with the following format:
+  // {"timestamp":"...","signals":[{"SLAVE_SCL_OUT":0},{"SLAVE_SDA_OUT":0},{"MASTER_SCL_OUT":0},{"MASTER_SDA_OUT":0}],"SCL":0,"SDA":1}
+  // Each line of the LOGIC_ANALYZER_FILE is a binary string with bits separated by spaces which are the output pins and the bus state (SCL and SDA)
+  // Example: 0 1 0 1 0 1 0 1
+
+  std::ifstream ifs(PIN_STATE_FILE);
+  std::string lastLine;
+  if (ifs.is_open()) {
+    std::string line;
+    while (std::getline(ifs, line)) {
+      lastLine = line;
+    }
+    ifs.close();
+  }
+
+  if(lastLine.size() > 0){
+    std::string timestamp;
+    std::map<std::string, int> pinStates;
+    int scl, sda;
+
+    rapidjson::Document d;
+    d.Parse(lastLine.c_str());
+    timestamp = d["timestamp"].GetString();
+    for(auto& signal : d["signals"].GetArray()){
+      pinStates[signal.GetObject().MemberBegin()->name.GetString()] = signal.GetObject().MemberBegin()->value.GetInt();
+    }
+
+    if(d.HasMember("SCL")){
+      scl = d["SCL"].GetInt();
+    } else {
+      scl = 0;
+    }
+
+    if(d.HasMember("SDA")){
+      sda = d["SDA"].GetInt();
+    } else {
+      sda = 0;
+    }
+
+    std::ofstream ofs(LOGIC_ANALYZER_FILE, std::ios::app);
+    if(ofs.is_open()){
+      ofs << sda << " " << scl << " " << pinStates["MASTER_SDA_OUT"] << " " << pinStates["MASTER_SCL_OUT"] << " " << pinStates["SLAVE_SDA_OUT"] << " " << pinStates["SLAVE_SCL_OUT"] << std::endl;
+      ofs.close();
+    } else {
+      std::cout << "Unable to open file for writing logic analyzer snapshot\n";
+    }
+  }
+}
+
+void startLogicAnalyzerProbe(std::chrono::milliseconds interval){  
+  while(logicAnalyzerProbeRunning){
+    logicAnalyzerSnapshot();
+    std::this_thread::sleep_for(interval);
+  }
+}
+
+void clearFile(std::string file){
+  std::ofstream ofs(file, std::ios::trunc);
+  ofs.close();
 }
 
 int main(int argc, char** argv){
   std::cout << "DesktopDemo Start.\n";
-
-  I2C_Role role = I2C_Role::MASTER;
 
   // Process command-line arguments
   for (int i = 1; i < argc; i++) {
@@ -36,6 +142,14 @@ int main(int argc, char** argv){
         return 1; // exit with error
       }
     }
+  }
+
+  // Clear the log files
+  std::string filename = std::string("output/") + (role == I2C_Role::MASTER ? "master" : "slave") + "_log.txt";
+  clearFile(filename);
+
+  if(role == I2C_Role::MASTER){
+    clearFile(PIN_STATE_FILE);
   }
 
   HAL_Pin sclOutPin, sdaOutPin, sclInPin, sdaInPin;
@@ -55,13 +169,22 @@ int main(int argc, char** argv){
     .sdaOutPin = HAL_pinSetup(&sdaOutPin, nullptr, 2, HAL_PullupConfig::PULLUP_ENABLE),
     .sclInPin = HAL_pinSetup(&sclInPin, nullptr, 3, HAL_PullupConfig::PULLUP_ENABLE),
     .sdaInPin = HAL_pinSetup(&sdaInPin, nullptr, 4, HAL_PullupConfig::PULLUP_ENABLE),
+    .timeUnit = 100,
     .print_str = &print_str,
     .print_num = &print_num,
   };
 
   I2C_init(&i2c_config);
 
+
+  if(role == I2C_Role::SLAVE) {
+    while(true){
+      I2C_read();
+    }
+  }
+
   while(true){
+
     // read user input from stdin and service commands for writing to the pins and reading from the pins
     std::cout << "> ";
     std::string input;
@@ -81,6 +204,19 @@ int main(int argc, char** argv){
       HAL_pinWrite(&sdaOutPin, HIGH);
     } else if(input == "write sda LOW"){
       HAL_pinWrite(&sdaOutPin, LOW);
+    } else if(input == "auto"){
+      clearFile(LOGIC_ANALYZER_FILE);
+      
+      logicAnalyzerProbeRunning = true;
+      std::thread t(startLogicAnalyzerProbe, std::chrono::milliseconds(10));
+      t.detach();
+
+      I2C_sendStartCondition();
+      I2C_write(52);
+      I2C_write(18);
+      I2C_sendStopCondition();
+
+      logicAnalyzerProbeRunning = false;
     } else {
       std::cout << "Invalid input\n";
       std::cout << "Valid inputs are:\n";
@@ -91,6 +227,7 @@ int main(int argc, char** argv){
       std::cout << "write scl LOW\n";
       std::cout << "write sda HIGH\n";
       std::cout << "write sda LOW\n";
+      std::cout << "auto\n";
     }
   }
   
